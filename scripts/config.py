bfb8bec0411ac84655b5ada667640288b86a9ad9
actually4NOT <49729056+actually4NOT@users.noreply.github.com>
2020-04-01 02:25:33 +0300

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "1071987485:AAFZGeEMVnFLgNXYStujmhkOKH3v13ENHa4"
-ADMIN_ID = 501797735
+ADMIN_ID = 0# your chat id is here
 MAIN_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 DB_NAME = "sender_db"
